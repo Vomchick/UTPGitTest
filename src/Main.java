496889d2b285dfc002d8e8bf74e8d52543b73556
Vomchick <vladislav.tavrel@gmail.com>
2024-10-11 16:19:27 +0200

    comment update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 // TODO: we need to add the missing classes!
 
+// Ok, I will add 'Adder' and s31792 will add 'Substractor'.
+
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
